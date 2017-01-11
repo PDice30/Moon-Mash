874092244e9ac990cf7469994397e3e69b8c3c2c
Phil PC <padeisinger@gmail.com>
2017-01-10 21:58:34 -0600

    Working with some planet rotations and initial testing

---
+++ Moon Mash/Assets/Scripts/PlanetRotation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotation : MonoBehaviour {
+
+	PlayerController playerController;
+	// Use this for initialization
+
+	void Awake() {
+		playerController = GameObject.Find ("PlayerController").GetComponent<PlayerController> () as PlayerController;
+	}
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//gameObject.transform.Rotate (new Vector3 (0, 0.2f, 0.2f));
+			
+	}
+	/*
+	void OnMouseDown() {
+		playerController.handlePlanetRotation(
+	}
+	*/
+}
---
+++ Moon Mash/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	Transform planet;
+	Vector3 currentTouchPos;
+	Vector3 previousTouchPos;
+
+	void Awake() {
+		planet = GameObject.Find ("TestPlanet").transform;
+	}
+
+	void Start () {
+		currentTouchPos = Vector3.zero;
+		previousTouchPos = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton ("Fire1")) {
+			handlePlanetRotation (Input.mousePosition);
+		}
+	}
+
+
+	public void handlePlanetRotation(Vector3 touchPos) {
+		//Debug.Log ("x = " + touchPos.x + " y = " + touchPos.y + " z = " + touchPos.z);
+		/*
+		if (currentTouchPos == Vector3.zero && previousTouchPos == Vector3.zero) {
+			currentTouchPos = touchPos;
+			previousTouchPos = touchPos;
+		}
+*/
+
+		//LUL HACKS
+
+		currentTouchPos = touchPos;
+		/*
+		Vector3 moveVector = currentTouchPos - previousTouchPos;
+		moveVector.Normalize ();
+		moveVector = new Vector3 (moveVector.y, moveVector.x, moveVector.z);
+		moveVector = moveVector * 5;
+		planet.Rotate (moveVector);
+*/
+
+		planet.transform.rotation = Quaternion.FromToRotation (previousTouchPos, currentTouchPos);
+
+		previousTouchPos = touchPos;
+	}
+}
