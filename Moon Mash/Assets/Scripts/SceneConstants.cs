b2ead849624413db2ad118c66edac09fb95bba85
DANNYSLAPTOP\Daniel <daniel.jashinsky@gmail.com>
2018-10-10 22:07:10 -0500

    Changing TerrainSpawner with more calculus and more trees

--- Moon Mash/Assets/Scripts/SceneConstants.cs
+++ Moon Mash/Assets/Scripts/SceneConstants.cs
@@ -5,7 +5,7 @@ namespace UnityEngine {
 	public class SceneConstants {
 
 		public const float TEST_ENEMY_SPAWN_TIME = 3.0f;
-		public const float TERRAIN_SPAWN_TIME = 1.0f;
+		public const float TERRAIN_SPAWN_TIME = .5f;
 		public const int NUMBER_OF_TERRAIN_TO_SPAWN = 20;
 	} 
 }
